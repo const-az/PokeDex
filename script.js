a87739cbdf0b902898549ce03e3afdb78e770323
Constanza Avilés Zavando <c.avileszavando@gmail.com>
2020-08-01 00:52:54 -0400

    Adapted to responsive design

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ $(document).ready(function(){
   })
 })
 
-// VARIABLES COLORES
-
 var colors={
   normal: "#deba77",
   fighting: "#c9a080",
@@ -61,10 +59,8 @@ var iconos={
   shadow: "fas fa-adjust",
 }
 
-// PARA CREAR SELECT CON TIPOS
 function typeSelect(){
   $.get("https://pokeapi.co/api/v2/type/", function(data){
-    console.log(data)
     let option = "";
     let type = data.results;
 
@@ -75,7 +71,6 @@ function typeSelect(){
       if (newOption.value != "dark" && newOption.value != "unknown" && newOption.value != "shadow" ) {
         $('#type-list')[0].options.add(newOption);
       }
-
     })
 
     $('#type-list').formSelect();
@@ -99,7 +94,7 @@ function putType(type){
         })
       }
 
-      imprimirTodos(pokemonTypes);
+      printAll(pokemonTypes);
 
     });
   });
@@ -110,54 +105,50 @@ function pokemonAll(){
   $.get("https://pokeapi.co/api/v2/pokedex/2", function(data){
   let pokemon = data.pokemon_entries;
   let card = "";
-  imprimirTodos(pokemon);
+  printAll(pokemon);
   })
 }
 
-// IMPRIMIR POKEMONES EN PANTALLA
-function imprimirTodos(pokemon) {
-  $("#poner-cartas")[0].innerHTML ="";
+function printAll(pokemon) {
+  $("#pokemon-cards")[0].innerHTML ="";
   pokemon.forEach(p => {
     card =
-          "<div class='col s1 min-carta-pokemon' id='min-carta-"+p.entry_number+"'>"+
+          "<div class='col s4 m2 xl1 min-carta-pokemon' id='min-carta-"+p.entry_number+"'>"+
             "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+p.entry_number+".png' onclick='clickPokemon("+p.entry_number+")'>"+
             "<p id='min-carta-name'>"+p.pokemon_species.name.charAt(0).toUpperCase() + p.pokemon_species.name.substring(1).toLowerCase()+"</p>"+
           "</div>";
 
-  $("#poner-cartas")[0].innerHTML += card;
+  $("#pokemon-cards")[0].innerHTML += card;
     });
 }
 
-// FUNCIÓN PARA EL SEARCH
 function searchPokemon() {
   var id = $("#searchPokemon")[0].value;
   if (id !== undefined && id!=="") {
-    imprimirPorId(id);
+    printById(id);
   } else {
     $("#searchPokemon")[0].value= ""
     pokemonAll();
     $('#carta').fadeOut();
   }
 }
-// FUNCIÓN PARA CERRAR LA CARD GRANDE
-function cerrarCard() {
+function closeCard() {
   $('#carta').fadeOut();
 }
 
-// FUNCIÓN PARA IMPRIMIR EL SEARCH
-function imprimirPorId(id) {
+function printById(id) {
   let card = "";
   $.get("https://pokeapi.co/api/v2/pokemon/" + id, data => {
-    $("#unPokemon")[0].innerHTML ="";
+    $("#singlePokemon")[0].innerHTML ="";
 
     card =
           "<div class='row' id='carta'>"+
-            "<button id='button-cerrar' onclick='cerrarCard()'><i class='fas fa-times'></i></button>"+
-            "<div class='col s3' id='carta-img'>"+
+            "<button id='button-cerrar' onclick='closeCard()'><i class='fas fa-times'></i></button>"+
+            "<div class='col s12 m6 l3' id='carta-img'>"+
               "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+data.id+".png'>"+
             "</div>"+
 
-            "<div class='col s4' id='carta-datos'>"+
+            "<div class='col s12 m6 l4' id='carta-datos'>"+
               "<h3 id='carta-nombre'>"+data.name.charAt(0).toUpperCase() + data.name.substring(1).toLowerCase()+"</h3>"+
               "<h5 id='carta-tipo'><i class='"+iconos[data.types[0].type.name]+"' style='color:"+colors[data.types[0].type.name]+"; margin-right: 10px;'></i>"+data.types[0].type.name+"</h5>"+
               "<div class='row' id='carta-datos'>"+
@@ -176,7 +167,7 @@ function imprimirPorId(id) {
               "</div>"+
             "</div>"+
 
-            "<div class='col s5' id='carta-graph'>"+
+            "<div class='col s12 l5' id='carta-graph'>"+
               "<div id='chartContainer' style='height: 230px; width: 100%;'></div>"+
             "</div>"+
           "</div>";
@@ -192,7 +183,7 @@ function imprimirPorId(id) {
       objeto={};
     }
 
-    $("#unPokemon")[0].innerHTML = card;
+    $("#singlePokemon")[0].innerHTML = card;
     graphPokemon(datosGraph,color)
     $('#carta').hide();
     $('#carta').fadeIn();
@@ -200,13 +191,11 @@ function imprimirPorId(id) {
   $("#searchPokemon")[0].value= ""
 }
 
-// CLICKEAR POKEMON
 function clickPokemon(id){
-  imprimirPorId(id);
+  printById(id);
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
-// GRAFICO
 function graphPokemon(datosGraph,color) {
 
 var chart = new CanvasJS.Chart("chartContainer", {
